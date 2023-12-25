f0168463e8bacd6b55415db234da32d45b31817d
AsHim Shrestha <ashimsth89@gmail.com>
2023-12-25 13:14:07 +0545

    post schema added

---
+++ server/mongoDB/models/post.js
@@ -0,0 +1,19 @@
+import mongoose, { model } from "mongoose";
+
+const postSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  prompt: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+});
+
+const Post = mongoose.model("Post", postSchema);    
+model.exports = Post;
