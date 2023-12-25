009255b8e6d2c5b2931d3bd005f3e6de395dfa97
AsHim Shrestha <ashimsth89@gmail.com>
2023-12-25 10:28:54 +0545

    mongodb connected

--- server/index.js
+++ server/index.js
@@ -1,5 +1,27 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./mongoDB/connect.js";
 dotenv.config();
+
+const app = express();
+app.use(cors());
+
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/", async (req, res) => {
+  res.send("Hello from DALL-E!");
+});
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGO_URL);
+    app.listen(8080, () => {
+      console.log("Server listening on port http://localhost:8080");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
